cb777482e09451e8102da1c68e0ef3989f52f449
Céline <celine@celine>
2018-03-25 18:39:04 +0200

    - retrieves all the information of a serie and tomes which compose it. It's now possible to retrieve all the series and its tomes.

--- parser.py
+++ parser.py
@@ -3,13 +3,13 @@ import urllib.request
 import string
 import pprint
 
-# ----------------- FONCTION QUI PARSE L'INDEX HTML DE TOUTES LES LETTRES DE L'ALPHABET -------------------- #
+# ----------------- FONCTION QUI PARSE L'INDEX HTML DE TOUTES LES LETTRES DE L'ALPHABET ET QUI RETOURNE LA LISTE DES SERIES -------------------- #
 def parseUrl(url) :
 #{
     # récupération du code source de la page html
     html = urllib.request.urlopen(url).read()
     # page html récupérée ci-dessus et mise au format 'beautiful soup'
-    soup = BeautifulSoup(html, "html.parser")
+    soup = BeautifulSoup(html, "html5lib")
 
     # récupération du tbody de la page html
     body = soup.table.tbody
@@ -30,6 +30,103 @@ def parseUrl(url) :
 #}
 # ----------------------------------------------------------------------------------------------------------- #
 
+
+# ----------------------------- FONCTION QUI PARSE LE CONTENU D'UN TOME ------------------------------------- #
+# -------------- RECUPERATION DES INFOS D'UN TOME EN PARCOURANT LA LISTE LI DU UL "entryInfos" ------------ #
+def parseTome(url_tomes) :
+#{
+    # récupération du code source de la page html
+    html = urllib.request.urlopen(url_tomes).read()
+    # page html récupérée ci-dessus et mise au format 'beautiful soup'
+    soup = BeautifulSoup(html, "html5lib")
+
+    # récupération de la liste des li contenu dans le ul="entryInfos"
+    # find_all retourne une liste de 1 seul car il n'y a qu'un seul "entryInfos" d'où le [0] pour éviter de faire un for sur un seul élément.
+    # on applique à nouveau le find_all sur les balises ('li') pour récupère une liste qui contient tous les li (> 0 élément)
+    listLi = soup.find_all(class_="entryInfos")[0].find_all('li')
+
+    datePublication = list(listLi)[10].meta['content']
+    # print("\nDate de publication : " + datePublication)
+
+    # récupération du résumé
+    resume = ""    
+    for child in soup.find(id="summary").p.children :
+        resume += str(child)
+    resume = resume.replace('<br/>', '\n')
+    # print("\nRésumé :  \n" + resume + "\n")
+
+    # récupération du lien de l'image
+    urlImage = soup.find(id="picinfo").a['href']
+    # print("Url image : " + urlImage) 
+
+    return datePublication, resume, urlImage
+#}
+# ----------------------------------------------------------------------------------------------------------- #
+
+
+# ---------------------------- RECUPERATION DE LA LISTE DES TOMES DE LA SERIE ------------------------------- #
+def recupTomesSerie(soup) : 
+#{
+    # récupération de la liste des tomes contenu dans "serieVolumes"
+    tomes = soup.find(id="serieVolumes").find_all(class_="vols")
+
+    # création d'une liste qui contient pour chaque élément l'url vers le tome, l'url vers l'image et le numéro du tome
+    listTomes = []
+
+    # print("Volumes : ")
+    for tome in tomes :
+        urlTome = tome.a['href']
+        urlImag = tome.a.img['src']
+        idVolume = tome.find_all(class_='selection')[0].string
+        # print(idVolume + " lien : "+ urlTome + " image : " + urlImag)
+        listTomes.append([urlTome, urlImag, idVolume])
+
+    return listTomes
+# }
+# ----------------------------------------------------------------------------------------------------------- #
+
+
+# -------------------------- RECUPERATION DU NOMBRE DE TOME EN FRANCE ET AU JAPON --------------------------- #
+def recupTomesVFVO(soup) :
+#{
+    # récupération des span contenu dans "numberblock"  
+    nbTomesVOVF = soup.find(id="numberblock").find_all("span")
+    # création d'une map du style [Key, [NbTomes, Statut]]
+    mapVOVF = {}
+    
+    # on parcourt tous les span
+    for span in nbTomesVOVF :
+        key = span.string
+        # on teste si la clé est égale à "VF"
+        if key == 'VF':
+            # récupération du nb de tomes
+            tomes = list(nbTomesVOVF)[0].contents[2].strip(' \t\r\n:')
+            # récupération du statut de la série (en cours, terminé, en pause)
+            status = list(nbTomesVOVF)[0].contents[3].string.strip(' \t\r\n:()')
+            # insertion de la clé et des valeurs associées dans la map
+            mapVOVF[key] = [tomes,status]
+        # on teste si la clé est égale à "VO"
+        if key == "VO":
+            # récupération du nb de tomes
+            tomes = list(nbTomesVOVF)[3].contents[1].contents[2].strip(' \t\r\n:')
+            # récupération du statut de la série (en cours, terminé, en pause)
+            status = list(nbTomesVOVF)[3].contents[1].contents[3].string.strip(' \t\r\n:()')
+            # insertion de la clé et des valeurs associées dans la map
+            mapVOVF[key] = [tomes, status]
+
+    # # si on trouve 'VO' dans la map alors on affiche la clé et ses valeurs
+    # if mapVOVF.get('VO') is not None:
+    #     print(" Tomes VO = " + mapVOVF['VO'][0] + " " + mapVOVF['VO'][1])
+        
+    # # si on trouve 'VF' dans la map alors on affiche la clé et ses valeurs
+    # if mapVOVF.get('VF') is not None:
+    #     print(" Tomes VF = " + mapVOVF['VF'][0] + " " + mapVOVF['VF'][1])
+
+    return mapVOVF
+# } 
+# ----------------------------------------------------------------------------------------------------------- #
+    
+
 # -------------- RECUPERATION DES INFOS DE LA SERIE EN PARCOURANT LA LISTE LI DU UL "entryInfos" ------------ #
 def recupInfosSerie(soup) :
 #{
@@ -38,101 +135,93 @@ def recupInfosSerie(soup) :
     # on applique à nouveau le find_all sur les balises ('li') pour récupère une liste qui contient tous les li (> 0 élément)
     listLi = soup.find_all(class_="entryInfos")[0].find_all('li')
 
-    # première ligne de la liste qui contient les li (converti en liste)
-    # on récupère le deuxième éléments du contenu de ce li sur lequel on enlève certains caractères spéciaux
-    titreVO = list(listLi)[0].contents[2].strip(' \t\r\n:')
-    print("Titre VO : " + titreVO)
+    mapInfosSerie = {} #map <clé,valeur>
+    infosInutiles = ["Illustration", "Code prix", "Code EAN"] #infos que je ne veux pas récupérer
 
-    titreTraduit = list(listLi)[1].contents[2].strip(' \t\r\n:')
-    print("Titre traduit :" + titreTraduit)
+    for li in listLi :
+        # récupération de la clé
+        key = li.strong.string.strip(' \t\n\r:')
 
-    dessinateur = list(listLi)[2].contents[2].string.strip(' \t\r\n:')
-    print("Dessinateur : " + dessinateur)
+        # if la clé fait partie des infos inutiles alors on passe au suivant
+        if li.strong.string in infosInutiles :
+            continue
+        elif li.strong.string == "Origine" : # cas spécial pour Origine car il y a des <span>
+            value = li.contents[1].string.strip(' \t\r\n:').replace(' ', '')
+        elif li.strong.string == "Genre" : # cas spécial pour genre qui peut possèder plusieurs genre
+            genres = ""
+            for child in li.find_all('a') :
+                genres += child.string.strip(' \t\r\n:') + ", "
+            value = genres
+        else : # récupération de value "par défaut"
+            value = li.contents[2].string.strip(' \t\n\r:')
 
-    scenariste = list(listLi)[3].contents[2].string.strip(' \t\r\n:')
-    print("Scénariste : " + scenariste)
+        # insertion de la clé et de la valeur dans la map
+        mapInfosSerie[key] = value
 
-    editeurVF = list(listLi)[4].contents[2].string.strip(' \t\r\n:')
-    print("Editeur VF : " + editeurVF)
 
-    typeManga = list(listLi)[6].contents[2].string.strip(' \t\r\n:')
-    print("Type : " + typeManga)
-
-    genres = ""
-    for child in list(listLi)[7].find_all('a') :
-        genres += child.string.strip(' \t\r\n:') + ", "
-    print("Genre : " + genres)
-
-    editeurVO = list(listLi)[8].contents[2].string.strip(' \t\r\n:')
-    print("Editeur VO : " + editeurVO)
-
-    origine = list(listLi)[11].contents[1].string.strip(' \t\r\n:').replace(' ', '')
-    print("Origine : " + origine)
+    return mapInfosSerie
 #}
 # ----------------------------------------------------------------------------------------------------------- #
 
-# -------------------------- RECUPERATION DU NOMBRE DE TOME EN FRANCE ET AU JAPON --------------------------- #
-def recupTomesVFVO(soup) :
-#{
-    nbTomesVOVF = soup.find(id="numberblock").find_all("span")
 
-    tomes_vf = list(nbTomesVOVF)[0].contents[2].strip(' \t\r\n:')
-    tomes_vo = list(nbTomesVOVF)[3].contents[1].contents[2].strip(' \t\r\n:')
-
-    print("Tomes VO : " + tomes_vo)
-    print("Tomes VF : " + tomes_vf)
-# }
-# ----------------------------------------------------------------------------------------------------------- #
-    
-# ---------------------------- RECUPERATION DE LA LISTE DES TOMES DE LA SERIE ------------------------------- #
-def recupTomesSerie(soup) : 
-#{
-    volumes = soup.find(id="serieVolumes").find_all(class_="vols")
-
-    print("Volumes : ")
-    for volume in volumes :
-        url_volume = volume.a['href']
-        url_imag = volume.a.img['src']
-        idVolume = volume.find_all(class_='selection')[0].string
-        print(idVolume + " lien : "+ url_volume + " image : " + url_imag)
-# }
-# ----------------------------------------------------------------------------------------------------------- #
-
-
-# ----------------------------- FONCTION QUI PARSE LE CONTENU D'UNE SERIE ----------------------------------- #
+# --------------------------------- RECUPERATION DU CONTENU D'UNE SERIE ------------------------------------- #
 def parseSerie(url) :
 #{
     # récupération du code source de la page html
     html = urllib.request.urlopen(url).read()
     # page html récupérée ci-dessus et mise au format 'beautiful soup'
-    soup = BeautifulSoup(html, "html.parser") 
+    soup = BeautifulSoup(html, "html5lib") 
+
+    mapInfosSerie = recupInfosSerie(soup)
+    print(" Infos Série : " + str(mapInfosSerie))
 
 
-    recupInfosSerie(soup)
-    recupTomesVFVO(soup)    
+    mapVOVF = recupTomesVFVO(soup) 
+    # si on trouve 'VO' dans la map alors on affiche la clé et ses valeurs
+    if mapVOVF.get('VO') is not None:
+        print(" Tomes VO = " + mapVOVF['VO'][0] + " " + mapVOVF['VO'][1])
+        
+    # si on trouve 'VF' dans la map alors on affiche la clé et ses valeurs
+    if mapVOVF.get('VF') is not None:
+        print(" Tomes VF = " + mapVOVF['VF'][0] + " " + mapVOVF['VF'][1])
 
-    resume = soup.find(id="summary").p.string
+
+    # récupération du résumé
+    resume = ""    
+    for child in soup.find(id="summary").p.children :
+        resume += str(child)
+    resume = resume.replace('<br/>', '\n')
     print("\nRésumé :  \n" + resume + "\n")
 
-    recupTomesSerie(soup)
+    listTomes = recupTomesSerie(soup)
+    print("Liste tomes : " + str(listTomes))
+    
+    # for url in urlTomes :
+    #     parseTome(url)
+    datePublication, resume, urlImage = parseTome(listTomes[17][0])
+    print("Date de publication du tome : " + datePublication)
+    print("Résumé du tome : " + resume)
+    print("Url de l'image du tome : " + urlImage)
 #}
-# ----------------------------------------------------------------------------------------------------------- #
+# ----------------------------------------------------------------------------------------------------------- #    
+
+
 
 
 # ----------------------------------------------------------------------------------------------------------- #
 #     Main     #
 
-#url principale
+# url principale
 indexSerie = 'https://www.manga-news.com/index.php/series/'
 bibliotheque = {}
 
 bibliotheque.update(parseUrl('https://www.manga-news.com/index.php/series/' + 'A'))
 
-#récupère une clé de la bibliothèque
-#transforme le type retourné par bibliotheque.keys() en type 'List'
+# récupère une clé de la bibliothèque
+# transforme le type retourné par bibliotheque.keys() en type 'List'
 key = list(bibliotheque.keys())[0]
-# print(key)
 
+# récupération de la liste des séries
 parseSerie(key)
 
 exit(0)
